f8da4680f5ca8b64ce1b70c7fdc3052e9a04e105
timotej2015 <timotej.kuzma2015@gmail.com>
2022-05-24 22:42:13 +0200

    Dodajanje uporabnikov s pravicami
    premaknil vse v backend in dodal addusers in removeusers

--- rain/backend/routes/paketnikRoutes.js
+++ rain/backend/routes/paketnikRoutes.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var paketnikController = require('../controllers/paketnikController.js');
-
-
 function requiresLogin(req,res,next){
     console.log("auth!");
     if(req.session && req.session.userId){
@@ -21,21 +19,19 @@ router.get('/', paketnikController.list);
 router.get('/dodaj', paketnikController.dodaj);
 router.get('/izbrisi', paketnikController.remove);
 router.get('/posodobi', paketnikController.update);
-
-/*
- * GET
- */
+router.get('/prikaziEnega', paketnikController.show);
+router.get('/brisiUser', paketnikController.deleteUsers);
+//tale mora biti pod vsemi ostalimi GETi
 router.get('/:id', paketnikController.show);
-
 /*
  * POST
  */
 router.post('/', paketnikController.create);
-
+router.post('/dodajPost', paketnikController.create);
+router.post('/dodajUser', paketnikController.addUsers);
 /*
  * PUT
  */
-
 /*
  * DELETE
  */
