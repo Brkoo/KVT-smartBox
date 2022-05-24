4c2a742509797be90e5fea59179731ae730d42f5
timotej2015 <timotej.kuzma2015@gmail.com>
2022-05-24 23:03:32 +0200

    Home
    Izgled zacetne strani

---
+++ rain/frontend/src/components/Home.js
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+import {Card, Carousel, Image} from 'react-bootstrap';
+import React from 'react';
+function Home(){
+    return (
+            <>
+                <h3>Dobrodošli na Direct4.me</h3>
+            </>
+    )
+
+}
+
+export default Home;
