c5ccc371b0a09d75ac9ebb04b3d46799a666b75c
Karlo Horvat <63784386+Brkoo@users.noreply.github.com>
2022-05-29 18:28:48 +0200

    Kostante in login
    Dodal activity za login, ter vse njene funkcije, ki so potrebne, da se user lahko logina

--- jeziki/app/src/main/java/com/example/utils/Constants.kt
+++ jeziki/app/src/main/java/com/example/utils/Constants.kt
@@ -2,7 +2,11 @@ package com.example.utils
 
 class Constans {
     companion object{
-        const val BASE_URL = "https://api-ms-stage.direct4.me/sandbox/v1/"
+        const val BASE_URL_OPEN_BOX = "https://api-ms-stage.direct4.me/sandbox/v1/Access/openbox"
         const val API_KEY = "9ea96945-3a37-4638-a5d4-22e89fbc998f"
+        const val BASE_URL = "http://172.20.10.8:3001/users/login2"
+        const val BASE_URL_REGISTER = "http://172.20.10.8:3001/users/register"
+        const val BASE_URL_OPEN = "http://172.20.10.8:3001/odklep/"
+        const val BASE_URL_GET_PREMMISION = "http://172.20.10.8:3001/paketnik/preveriDostop"
     }
 }
